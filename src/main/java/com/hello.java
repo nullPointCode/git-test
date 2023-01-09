85670c12439b2314d3f93da58d3e3396bac88449
hjh <407047681@qq.com>
2023-01-09 11:19:07 +0800

    wwj second commit

--- src/main/java/com/hello.java
+++ src/main/java/com/hello.java
@@ -5,5 +5,6 @@ public class hello {
         System.out.println("hello world!!!");
         System.out.println("hello git!!!");
         System.out.println("hello wwj!!!");
+        System.out.println("hello change01");
     }
 }
